a74932e7ec6fc7b0ebc448e5842220887e2a9184
Sean Carley <milythael.mourning@gmail.com>
2016-08-07 18:01:38 -0400

    test the basic hex tile connections

--- spec/hex_tile_spec.rb
+++ spec/hex_tile_spec.rb
@@ -21,5 +21,77 @@ RSpec.describe HexTile do
     it "right is connected" do
       expect(elbow.side(:right).connected).to be true
     end
+
+    it "bottom right is not connected" do
+      expect(elbow.side(:bottom_right).connected).to be false
+    end
+
+    it "bottom left is not connected" do
+      expect(elbow.side(:bottom_left).connected).to be false
+    end
+
+    it "left is not connected" do
+      expect(elbow.side(:left).connected).to be false
+    end
+
+    it "top left is not connected" do
+      expect(elbow.side(:top_left).connected).to be false
+    end
+  end
+
+  describe "bend" do
+    let(:bend) { HexTile.bend }
+
+    it "top right is connected" do
+      expect(bend.side(:top_right).connected).to be true
+    end
+
+    it "right is not connected" do
+      expect(bend.side(:right).connected).to be false
+    end
+
+    it "bottom right is connected" do
+      expect(bend.side(:bottom_right).connected).to be true
+    end
+
+    it "bottom left is not connected" do
+      expect(bend.side(:bottom_left).connected).to be false
+    end
+
+    it "left is not connected" do
+      expect(bend.side(:left).connected).to be false
+    end
+
+    it "top left is not connected" do
+      expect(bend.side(:top_left).connected).to be false
+    end
+  end
+
+  describe "straight" do
+    let(:straight) { HexTile.straight }
+
+    it "top right is connected" do
+      expect(straight.side(:top_right).connected).to be true
+    end
+
+    it "right is not connected" do
+      expect(straight.side(:right).connected).to be false
+    end
+
+    it "bottom right is not connected" do
+      expect(straight.side(:bottom_right).connected).to be false
+    end
+
+    it "bottom left is connected" do
+      expect(straight.side(:bottom_left).connected).to be true
+    end
+
+    it "left is not connected" do
+      expect(straight.side(:left).connected).to be false
+    end
+
+    it "top left is not connected" do
+      expect(straight.side(:top_left).connected).to be false
+    end
   end
 end
