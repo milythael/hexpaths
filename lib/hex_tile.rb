26b233b85570dbdfbd634baa48cb35da31c1f436
Sean Carley <milythael.mourning@gmail.com>
2016-08-07 17:52:56 -0400

    create sides when a hex tile is constructed

--- lib/hex_tile.rb
+++ lib/hex_tile.rb
@@ -1,7 +1,42 @@
 require 'tile'
 class HexTile < Tile
-  def initialize
-    super
-    @sides = [nil, nil, nil, nil, nil, nil]
+  def initialize(top_right = false, right = false, bottom_right = false, bottom_left = false, left = false, top_left = false)
+    super()
+    @sides = [
+      HexSide.new(:top_right, top_right),
+      HexSide.new(:right, right),
+      HexSide.new(:bottom_right, bottom_right),
+      HexSide.new(:bottom_left, bottom_left),
+      HexSide.new(:left, left),
+      HexSide.new(:top_left, top_left)
+    ]
+  end
+
+  def self.elbow
+    HexTile.new(true, true)
+  end
+
+  def self.bend
+    HexTile.new(true, false, true)
+  end
+
+  def self.straight
+    HexTile.new(true, false, false, true)
+  end
+
+  def self.side_names
+    HexSide.side_names
+  end
+
+  def side_names
+    self.class.side_names
+  end
+
+  def side(index)
+    if index.is_a? Integer
+      sides[index]
+    else
+      sides[side_names.find_index(index)]
+    end
   end
 end
--- spec/hex_tile_spec.rb
+++ spec/hex_tile_spec.rb
@@ -1,6 +1,6 @@
 require 'hex_tile'
 RSpec.describe HexTile do
-  describe "Blank Hex Tile" do
+  describe "blank hex tile" do
     let(:blank) { HexTile.new }
     it "has no paths" do
       expect(blank.paths.length).to eq 0
@@ -8,6 +8,18 @@ RSpec.describe HexTile do
 
     it "has 6 sides" do
       expect(blank.sides.length).to eq 6
+    end
+  end
+
+  describe "elbow" do
+    let(:elbow) { HexTile.elbow }
+
+    it "top right is connected" do
+      expect(elbow.side(:top_right).connected).to be true
+    end
+
+    it "right is connected" do
+      expect(elbow.side(:right).connected).to be true
     end
   end
 end
